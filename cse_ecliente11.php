ecab47fff69f24dce8328bfcdddfe4c936ff3f25
Jaime.Delgado <jaime.delgado@katary.co>
2026-06-07 11:15:17 -0500

    ajuste de validación de correo
    ajuste de validación de correo

--- cse_ecliente11.php
+++ cse_ecliente11.php
@@ -11,22 +11,65 @@
 
 <!-- Funcion que valida que no queden en blanco los campos obligatorios -->
 <script languaje="javascript">
-function validar()
+async function validar()
 {
-var error = "Por favor, para continuar,\ncomplete los siguientes campos:\n\n";
-var a = ""
-    if (document.form1.tpid_cli.value == "") { a += " Tipo de Documento de Identificación\n"; }
-    if (document.form1.nrod_cli.value == "") { a += " Numero de Identificación\n"; }
-    if (document.form1.nomb_cli.value == "") { a += " Nombres\n"; }
-    if (document.form1.apel_cli.value == "") { a += " Apellidos\n"; }
-    //if (document.form1.sexo_cli.value == "") { a += " Sexo\n"; }
-    //if (document.form1.fnac_cli.value == "") { a += " Fecha de Nacimiento\n"; }
-    //if (document.form1.tele_cli.value == "") { a += " Teléfono\n"; }
-    if (a != "") 
-    { alert(error + a);return true;}
+  var error = "Por favor, para continuar,\ncomplete los siguientes campos:\n\n";
+  var a = ""
+  if (document.form1.tpid_cli.value == "") { a += " Tipo de Documento de Identificación\n"; }
+  if (document.form1.nrod_cli.value == "") { a += " Numero de Identificación\n"; }
+  if (document.form1.nomb_cli.value == "") { a += " Nombres\n"; }
+  if (document.form1.apel_cli.value == "") { a += " Apellidos\n"; }
+  //if (document.form1.sexo_cli.value == "") { a += " Sexo\n"; }
+  //if (document.form1.fnac_cli.value == "") { a += " Fecha de Nacimiento\n"; }
+  //if (document.form1.tele_cli.value == "") { a += " Teléfono\n"; }
 
-document.form1.submit()
+  var emai_cli = document.form1.emai_cli.value;
+
+  const existe = await existeEmail(emai_cli);
+
+  if (existe === true) {
+      a += " El correo electrónico ya existe en la base de datos\n";
+  }
+
+
+  if (a != "") 
+  { alert(error + a);return true;}
+  
+  document.form1.submit()
 }
+
+async function existeEmail(emai_cli){
+    var codigo = document.form1.codi_cli.value;
+    
+    const datos = {
+        opcion: 'existeEmail',
+        email: emai_cli,
+        codigo: codigo
+    };
+
+    try {
+        const response = await fetch("crudCliente.php", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(datos)
+        });
+
+        if (!response.ok) throw new Error(`Error HTTP: ${response.status}`);
+
+        const data = await response.json();
+        
+        return data.success; // true si existe, false si no existe
+
+    } catch (error) {
+        console.error('Falló la petición:', error);
+        alert('Error de conexión, intente de nuevo.');
+    } finally {
+        //btn.disabled = false;
+        //btn.textContent = 'Enviar';
+    }
+}
+
+
 function atras()
 {
   history.go(-1)
